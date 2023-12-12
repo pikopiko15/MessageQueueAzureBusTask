2a15e7c726d81651d35a991ce2bce7b9bddb05c2
Kemal Maralov <Kemal_Maralov@epam.com>
2023-12-12 16:35:47 +0600

    Add project files.

---
+++ QueueReceiver/Program.cs
@@ -0,0 +1,69 @@
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        ServiceBusClient client;
+
+        ServiceBusProcessor processor;
+
+        var clientOptions = new ServiceBusClientOptions()
+        {
+            TransportType = ServiceBusTransportType.AmqpWebSockets
+        };
+        client = new ServiceBusClient("messagequeuetaskservicebus.servicebus.windows.net",
+            new DefaultAzureCredential(), clientOptions);
+
+        processor = client.CreateProcessor("imagesqueue", new ServiceBusProcessorOptions());
+
+        try
+        {
+            processor.ProcessMessageAsync += MessageHandler;
+
+            processor.ProcessErrorAsync += ErrorHandler;
+
+            await processor.StartProcessingAsync();
+
+            Console.WriteLine("Wait for a minute and then press any key to end the processing");
+            Console.ReadKey();
+
+            Console.WriteLine("\nStopping the receiver...");
+            await processor.StopProcessingAsync();
+            Console.WriteLine("Stopped receiving messages");
+        }
+        finally
+        {
+            // Calling DisposeAsync on client types is required to ensure that network
+            // resources and other unmanaged objects are properly cleaned up.
+            await processor.DisposeAsync();
+            await client.DisposeAsync();
+        }
+
+        async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            
+
+            byte[] chunkData = args.Message.Body.ToArray();
+
+            // Append the chunk to the output file
+            using (var fileStream = new FileStream(@"C:\Users\Kemal_Maralov\Desktop\images\receivedimage.jpg", FileMode.Append))
+            {
+                await fileStream.WriteAsync(chunkData, 0, chunkData.Length);
+            }
+
+            // complete the message. message is deleted from the queue. 
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ QueueSender/Program.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        ServiceBusClient client;
+
+        ServiceBusSender sender;
+
+        var clientOptions = new ServiceBusClientOptions
+        {
+            TransportType = ServiceBusTransportType.AmqpWebSockets
+        };
+
+        client = new ServiceBusClient(
+            "messagequeuetaskservicebus.servicebus.windows.net",
+            new DefaultAzureCredential(),
+            clientOptions);
+        sender = client.CreateSender("imagesqueue");
+
+        await SendChunksToServiceBus(@"C:\Users\Kemal_Maralov\Desktop\images\annie-spratt-wW-1-BRNbHM-unsplash.jpg", client, sender);
+
+        Console.WriteLine("Press any key to end the application");
+        Console.ReadKey();
+    }
+
+    private static async Task SendChunksToServiceBus(string filePath, ServiceBusClient serviceBusClient, ServiceBusSender sender)
+    {
+        try
+        {
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                byte[] buffer = new byte[128 * 1024];
+                int bytesRead;
+
+                while ((bytesRead = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    byte[] chunkData = new byte[bytesRead];
+                    Array.Copy(buffer, chunkData, bytesRead);
+
+                    var message = new ServiceBusMessage(chunkData);
+                    await sender.SendMessageAsync(message);
+                }
+            }
+
+            Console.WriteLine("File chunks sent to Service Bus successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
